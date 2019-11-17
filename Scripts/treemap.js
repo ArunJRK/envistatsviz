74cec4095b97e507d8022116e87350e3063e0457
ArunJRK <arun.jrk99@gmail.com>
2019-11-17 11:20:38 +0530

    Minification

--- Scripts/treemap.js
+++ Scripts/treemap.js
@@ -1,3 +1,4 @@
+am4core.useTheme(am4themes_animated);
 var soiltype = am4core.create("soiltype", am4charts.TreeMap);
 soiltype.paddingRight = 20;
 
@@ -44,6 +45,7 @@ function drawtree() {
   //Defining the anatomy of soiltype
   var level1 = soiltype.seriesTemplates.create("0");
   var level1_column = level1.columns.template;
+  
   level1_column.column.cornerRadius(10, 10, 10, 10);
   level1_column.fillOpacity = 0.8;
   level1_column.stroke = am4core.color("#fff");
@@ -51,6 +53,16 @@ function drawtree() {
   level1_column.strokeOpacity = 1;
   soilchartdata = [];
   console.log(soilchartdata);
+
+  var level1seriestemplate = soiltype.seriesTemplates.create("1");
+  level1seriestemplate.columns.template.fillOpacity = 1;
+
+  var bullet1 = level1seriestemplate.bullets.push(new am4charts.LabelBullet());
+  bullet1.locationX = 0.5;
+  bullet1.locationY = 0.5;
+  bullet1.label.text = "{name}";
+  bullet1.label.fill = am4core.color("#ffffff");
+
 }
 
 //Chart 2
@@ -87,3 +99,95 @@ function drawtree2() {
   level1_column.strokeOpacity = 1;
   //console.log("drewdegradedland");
 }
+
+
+// Chart 3
+
+var degradationtype = am4core.create("degradationtype", am4charts.TreeMap);
+var degradationtypedata;
+$.getJSON("Data/Degradation-by-type-2011-13 copy.json", function(data) {
+  ////console.log('success');
+  degradationtypedata = data;
+  //console.log(soilmaindata);
+
+  drawtree3();
+});
+
+function drawtree3(){
+
+// only one level visible initially
+degradationtype.maxLevels = 1;
+// define data fields
+degradationtype.dataFields.value = "area";
+degradationtype.dataFields.name = "State";
+degradationtype.dataFields.children = "Data";
+degradationtype.homeText = "Degradation By type";
+
+// enable navigation
+degradationtype.navigationBar = new am4charts.NavigationBar();
+
+// level 0 series template
+var level0SeriesTemplate = degradationtype.seriesTemplates.create("0");
+level0SeriesTemplate.strokeWidth = 2;
+
+// by default only current level series bullets are visible, but as we need brand bullets to be visible all the time, we modify it's hidden state
+level0SeriesTemplate.bulletsContainer.hiddenState.properties.opacity = 1;
+level0SeriesTemplate.bulletsContainer.hiddenState.properties.visible = true;
+
+//Insert Names
+level1SeriesTemplate.columns.template.fillOpacity = 0;
+
+var bullet0 = level0SeriesTemplate.bullets.push(new am4charts.LabelBullet());
+bullet0.locationX = 0.5;
+bullet0.locationY = 0.5;
+bullet0.label.text = "{name}";
+bullet0.label.fill = am4core.color("#ffffff");
+// create hover state
+var columnTemplate = level0SeriesTemplate.columns.template;
+var hoverState = columnTemplate.states.create("hover");
+
+// darken
+hoverState.adapter.add("fill", function (fill, target) {
+	if (fill instanceof am4core.Color) {
+		return am4core.color(am4core.colors.brighten(fill.rgb, -0.2));
+	}
+	return fill;
+})
+
+// add logo
+// var image = columnTemplate.createChild(am4core.Image);
+// image.opacity = 0.15;
+// image.align = "center";
+// image.valign = "middle";
+// image.width = am4core.percent(80);
+// image.height = am4core.percent(80);
+
+// // add adapter for href to load correct image
+// image.adapter.add("href", function (href, target) {
+// 	var dataItem = target.parent.dataItem;
+// 	if (dataItem) {
+// 		return "logos/" + dataItem.treeMapDataItem.name.toLowerCase() + ".png";
+// 	}
+// });
+
+// level1 series template
+var level1SeriesTemplate = degradationtype.seriesTemplates.create("1");
+level1SeriesTemplate.columns.template.fillOpacity = 0;
+
+var bullet1 = level1SeriesTemplate.bullets.push(new am4charts.LabelBullet());
+bullet1.locationX = 0.5;
+bullet1.locationY = 0.5;
+bullet1.label.text = "{name}";
+bullet1.label.fill = am4core.color("#ffffff");
+
+// level2 series template
+var level2SeriesTemplate = degradationtype.seriesTemplates.create("2");
+level2SeriesTemplate.columns.template.fillOpacity = 0;
+
+var bullet2 = level2SeriesTemplate.bullets.push(new am4charts.LabelBullet());
+bullet2.locationX = 0.5;
+bullet2.locationY = 0.5;
+bullet2.label.text = "{name}";
+bullet2.label.fill = am4core.color("#ffffff");
+
+degradationtype.data = degradationtypedata;}
